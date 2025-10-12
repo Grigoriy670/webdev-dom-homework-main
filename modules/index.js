7fb80522af7ea2bca94616290d2ba5dfd10c6132
Grigoriy670 <smerte.ru@mail.ru>
2025-10-12 09:44:51 +0700

    Prettier

--- modules/index.js
+++ modules/index.js
@@ -1,34 +1,11 @@
-import { addComment, getSortedComments } from "./comments.js";
-import { renderComments } from "./render.js";
+import { addComment, getSortedComments } from './comments.js'
+import { renderComments } from './render.js'
+import {rerender} from './render.js'
 
-const authorInput = document.getElementById("author");
-const commentTextInput = document.getElementById("comment-text");
-const addCommentBtn = document.getElementById("add-comment");
-const commentsContainer = document.getElementById("comments-container");
-const log = document.getElementById("log");
+const authorInput = document.getElementById('author')
+const commentTextInput = document.getElementById('comment-text')
+const addCommentBtn = document.getElementById('add-comment')
+const commentsContainer = document.getElementById('comments-container')
+const log = document.getElementById('log')
 
-function rerender() {
-  renderComments(getSortedComments(), commentsContainer, commentTextInput);
-}
 
-addCommentBtn.addEventListener("click", () => {
-  const author = authorInput.value.trim();
-  const text = commentTextInput.value.trim();
-  const newComment = addComment(author, text);
-  if (newComment) {
-    authorInput.value = "";
-    commentTextInput.value = "";
-    rerender();
-  } else {
-    alert("Пожалуйста, заполните все поля");
-  }
-});
-
-commentTextInput.addEventListener("keypress", (e) => {
-  if (e.key === "Enter" && e.ctrlKey) addCommentBtn.click();
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-  log.textContent += "Страница загружена полностью!\n";
-  rerender();
-});
--- modules/render.js
+++ modules/render.js
@@ -1,24 +1,24 @@
-import { toggleLike } from "./comments.js";
+import { toggleLike } from './comments.js'
 
 export function formatDate(dateString) {
-  return new Date(dateString).toLocaleString("ru-RU", {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
-  });
+    return new Date(dateString).toLocaleString('ru-RU', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+    })
 }
 
 export function renderComments(comments, container, commentTextInput) {
-  container.innerHTML = "";
+    container.innerHTML = ''
 
-  comments.forEach((comment) => {
-    const li = document.createElement("li");
-    li.className = "comment";
-    li.dataset.id = comment.id;
+    comments.forEach((comment) => {
+        const li = document.createElement('li')
+        li.className = 'comment'
+        li.dataset.id = comment.id
 
-    li.innerHTML = `
+        li.innerHTML = `
       <div class="comment-header">
         <div>${comment.author}</div>
         <div>${formatDate(comment.date)}</div>
@@ -30,24 +30,50 @@ export function renderComments(comments, container, commentTextInput) {
         <div class="likes">
           <span class="likes-counter">${comment.likes}</span>
           <button class="like-button ${
-            comment.liked ? "-active-like" : ""
+              comment.liked ? '-active-like' : ''
           }"></button>
         </div>
       </div>
-    `;
+    `
 
-    li.addEventListener("click", () => {
-      commentTextInput.value = `@${comment.author}, ${comment.text}\n`;
-      commentTextInput.focus();
-    });
+        li.addEventListener('click', () => {
+            commentTextInput.value = `@${comment.author}, ${comment.text}\n`
+            commentTextInput.focus()
+        })
 
-    
-    li.querySelector(".like-button").addEventListener("click", (e) => {
-      e.stopPropagation();
-      toggleLike(comment.id);
-      renderComments(comments, container, commentTextInput);
-    });
+        li.querySelector('.like-button').addEventListener('click', (e) => {
+            e.stopPropagation()
+            toggleLike(comment.id)
+            renderComments(comments, container, commentTextInput)
+        })
 
-    container.appendChild(li);
-  });
+        container.appendChild(li)
+    })
 }
+
+
+export function rerender() {
+    renderComments(getSortedComments(), commentsContainer, commentTextInput)
+}
+
+addCommentBtn.addEventListener('click', () => {
+    const author = authorInput.value.trim()
+    const text = commentTextInput.value.trim()
+    const newComment = addComment(author, text)
+    if (newComment) {
+        authorInput.value = ''
+        commentTextInput.value = ''
+        rerender()
+    } else {
+        alert('Пожалуйста, заполните все поля')
+    }
+})
+
+commentTextInput.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter' && e.ctrlKey) addCommentBtn.click()
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    log.textContent += 'Страница загружена полностью!\n'
+    rerender()
+})
